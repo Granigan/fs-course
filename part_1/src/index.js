00d71bf04df7c9a02d8b0ac97c0289a857a88465
Tero Tapio <teromarkustapio@gmail.com>
2019-01-16 13:40:45 +0200

    1.1 divides into components

--- part_1/src/index.js
+++ part_1/src/index.js
@@ -1,6 +1,41 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Header = (props) => {
+  return (
+    <div>
+      <h1>
+        {props.course}
+      </h1>
+    </div>
+  )
+}
+
+const Content = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return (
+    <div>
+      <p>
+        Yhteensä {props.exercises1 + props.exercises2 + props.exercises3} tehtävää.
+      </p>
+    </div>
+  )
+}
 const App = () => {
   const course = 'Half Stack -sovelluskehitys'
   const part1 = 'Reactin perusteet'
@@ -12,19 +47,12 @@ const App = () => {
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>
-        Yhteensä {exercises1 + exercises2 + exercises3} tehtävää.
-      </p>
+      <Header course={course} />
+      <Content part1 = {part1} part2 = {part2} part3 = {part3} 
+        exercises1 = {exercises1} exercises2 = {exercises2} 
+        exercises3 = {exercises3} />
+      <Total exercises1 = {exercises1} exercises2 = {exercises2} 
+        exercises3 = {exercises3} />
     </div>
     
   )
