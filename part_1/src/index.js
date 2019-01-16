dea6a093afa70a38de5c04c7c0e6f3569c157bd5
Tero Tapio <teromarkustapio@gmail.com>
2019-01-16 13:27:14 +0200

    ready for tasks

--- part_1/src/index.js
+++ part_1/src/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => (
-  <div>
-    <p>Hello world</p>
-  </div>
-)
+const App = () => {
+  const course = 'Half Stack -sovelluskehitys'
+  const part1 = 'Reactin perusteet'
+  const exercises1 = 10
+  const part2 = 'Tiedonvälitys propseilla'
+  const exercises2 = 7
+  const part3 = 'Komponenttien tila'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <h1>{course}</h1>
+      <p>
+        {part1} {exercises1}
+      </p>
+      <p>
+        {part2} {exercises2}
+      </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>
+        Yhteensä {exercises1 + exercises2 + exercises3} tehtävää.
+      </p>
+    </div>
+    
+  )
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
