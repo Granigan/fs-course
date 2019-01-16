298b3461e8205a0f8e52e0fb3c20c471dee60f5f
Tero Tapio <teromarkustapio@gmail.com>
2019-01-16 13:43:34 +0200

    1.2 more components

--- part_1/src/index.js
+++ part_1/src/index.js
@@ -14,15 +14,9 @@ const Header = (props) => {
 const Content = (props) => {
   return (
     <div>
-      <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+      <Part part = {props.part1} exercises = {props.exercises1} />
+      <Part part = {props.part2} exercises = {props.exercises2} />
+      <Part part = {props.part3} exercises = {props.exercises3} />
     </div>
   )
 }
@@ -36,6 +30,17 @@ const Total = (props) => {
     </div>
   )
 }
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part} {props.exercises}
+      </p>
+    </div>
+  )
+}
+
 const App = () => {
   const course = 'Half Stack -sovelluskehitys'
   const part1 = 'Reactin perusteet'
