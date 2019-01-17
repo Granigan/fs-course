5e98c75756a86a5d0b65f433b57a93dd903be3d4
Tero Tapio <teromarkustapio@gmail.com>
2019-01-17 18:19:19 +0200

    1.6 unicafe osa1

--- part_1_unicafe/src/index.js
+++ part_1_unicafe/src/index.js
@@ -1,31 +1,31 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
-const Button = ({}) => {
-  
-}
+const Button = props => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
 
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   return (
     <div>
       <h1>Anna palautetta!</h1>
       <div>
-        <button>Hyvä</button>
-        <button>Neutraali</button>
-        <button>Huono</button>
+        <Button handleClick={() => setGood(good + 1)} text="Hyvä" />
+        <Button handleClick={() => setNeutral(neutral + 1)} text="Neutraali" />
+        <Button handleClick={() => setBad(bad + 1)} text="Huono" />
       </div>
       <h1>Tilastot</h1>
       <div>
         <p>Hyvä: {good}</p>
         <p>Neutraali: {neutral}</p>
         <p>Huono: {bad}</p>
-        </div>
+      </div>
     </div>
-  )
+  );
 };
 
 ReactDOM.render(<App />, document.getElementById("root"));
