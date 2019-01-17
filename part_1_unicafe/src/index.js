f4beb1452f19c4c8c3b9cae142e7743a08306794
Tero Tapio <teromarkustapio@gmail.com>
2019-01-17 19:09:01 +0200

    1.9&1.10 unicafe osat 4&5

--- part_1_unicafe/src/index.js
+++ part_1_unicafe/src/index.js
@@ -5,27 +5,25 @@ const Button = props => {
   return <button onClick={props.handleClick}>{props.text}</button>;
 };
 
-const Stats = props => {
-  const total = props.good + props.neutral + props.bad;
-
+const Statistics = props => {
+  if (props.total === 0) {
+    return <p>Yhtään palautetta ei ole vielä annettu.</p>;
+  }
   return (
     <div>
-      <h1>Tilastot</h1>
-      <div>
-        <p>
-          Hyvä: {props.good}
-          <br />
-          Neutraali: {props.neutral}
-          <br />
-          Huono: {props.bad}
-          <br />
-          Yhteensä: {total}
-          <br />
-          Keskiarvo: {((props.good - props.bad) / total).toFixed(2)}
-          <br />
-          Positiivisia: {((props.good / total) * 100).toFixed(2)} %<br />
-        </p>
-      </div>
+      <p>
+        Hyvä: {props.good}
+        <br />
+        Neutraali: {props.neutral}
+        <br />
+        Huono: {props.bad}
+        <br />
+        Yhteensä: {props.total}
+        <br />
+        Keskiarvo: {((props.good - props.bad) / props.total).toFixed(2)}
+        <br />
+        Positiivisia: {((props.good / props.total) * 100).toFixed(2)} %<br />
+      </p>
     </div>
   );
 };
@@ -33,6 +31,7 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
+  const total = good + neutral + bad;
 
   return (
     <div>
@@ -41,7 +40,10 @@ const App = () => {
         <Button handleClick={() => setGood(good + 1)} text="Hyvä" />
         <Button handleClick={() => setNeutral(neutral + 1)} text="Neutraali" />
         <Button handleClick={() => setBad(bad + 1)} text="Huono" />
-        <Stats good={good} neutral={neutral} bad={bad} />
+      </div>
+      <h1>Tilastot</h1>
+      <div>
+        <Statistics good={good} neutral={neutral} bad={bad} total={total} />
       </div>
     </div>
   );
