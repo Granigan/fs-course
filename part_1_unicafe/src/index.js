fa2d8309d3c8bed1d498478bee33729ebcb9a805
Tero Tapio <teromarkustapio@gmail.com>
2019-01-17 17:07:19 +0200

    splits part 1 into two projects

---
+++ part_1_unicafe/src/index.js
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+  return (
+    <div>tba</div>
+  )
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
