84910d8ac607ced7bb85ea819eaa02cf65893dda
Tero Tapio <teromarkustapio@gmail.com>
2019-01-17 17:44:33 +0200

    layout

--- part_1_unicafe/src/index.js
+++ part_1_unicafe/src/index.js
@@ -1,13 +1,30 @@
 import React, { useState } from "react";
 import ReactDOM from "react-dom";
 
+const Button = ({}) => {
+  
+}
+
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
   return (
-    <div>tba</div>
+    <div>
+      <h1>Anna palautetta!</h1>
+      <div>
+        <button>Hyvä</button>
+        <button>Neutraali</button>
+        <button>Huono</button>
+      </div>
+      <h1>Tilastot</h1>
+      <div>
+        <p>Hyvä: {good}</p>
+        <p>Neutraali: {neutral}</p>
+        <p>Huono: {bad}</p>
+        </div>
+    </div>
   )
 };
 
