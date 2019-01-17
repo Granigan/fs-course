338eafbb8e726212738c075433febe663c3a7ee1
Tero Tapio <teromarkustapio@gmail.com>
2019-01-17 18:34:47 +0200

    1.8 unicafe osa3

--- part_1_unicafe/src/index.js
+++ part_1_unicafe/src/index.js
@@ -5,11 +5,34 @@ const Button = props => {
   return <button onClick={props.handleClick}>{props.text}</button>;
 };
 
+const Stats = props => {
+  const total = props.good + props.neutral + props.bad;
+
+  return (
+    <div>
+      <h1>Tilastot</h1>
+      <div>
+        <p>
+          Hyvä: {props.good}
+          <br />
+          Neutraali: {props.neutral}
+          <br />
+          Huono: {props.bad}
+          <br />
+          Yhteensä: {total}
+          <br />
+          Keskiarvo: {((props.good - props.bad) / total).toFixed(2)}
+          <br />
+          Positiivisia: {((props.good / total) * 100).toFixed(2)} %<br />
+        </p>
+      </div>
+    </div>
+  );
+};
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  const total = good + neutral + bad;
 
   return (
     <div>
@@ -18,16 +41,7 @@ const App = () => {
         <Button handleClick={() => setGood(good + 1)} text="Hyvä" />
         <Button handleClick={() => setNeutral(neutral + 1)} text="Neutraali" />
         <Button handleClick={() => setBad(bad + 1)} text="Huono" />
-      </div>
-      <h1>Tilastot</h1>
-      <div>
-        <p>Hyvä: {good}<br />
-        Neutraali: {neutral}<br />
-        Huono: {bad}<br />
-        Yhteensä: {total}<br/>
-        Keskiarvo: {((good - bad)/total).toFixed(2)}<br />
-        Positiivisia: {(good/total*100).toFixed(2)} %<br />
-        </p>
+        <Stats good={good} neutral={neutral} bad={bad} />
       </div>
     </div>
   );
