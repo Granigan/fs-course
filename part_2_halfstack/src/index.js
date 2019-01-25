c74006d1747abb47b886f03ed20edb4cb861e4b7
Tero Tapio <teromarkustapio@gmail.com>
2019-01-25 17:36:13 +0200

    2.1 first map

--- part_2_halfstack/src/index.js
+++ part_2_halfstack/src/index.js
@@ -1,12 +1,62 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const Course = ({ course }) => (
+  <div>
+    <Header name={course.name} />
+    <Content parts={course.parts} />
+  </div>
+);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const Header = ({ name }) => <h1>{name}</h1>;
+
+const Total = props => {
+  const total =
+    props.parts[0].exercises +
+    props.parts[1].exercises +
+    props.parts[2].exercises;
+
+  return <p>yhteensä {total} tehtävää</p>;
+};
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map(part => (
+      <Part part={part} />
+    ))}
+  </div>
+);
+
+const App = () => {
+  const course = {
+    name: "Half Stack -sovelluskehitys",
+    parts: [
+      {
+        name: "Reactin perusteet",
+        exercises: 10
+      },
+      {
+        name: "Tiedonvälitys propseilla",
+        exercises: 7
+      },
+      {
+        name: "Komponenttien tila",
+        exercises: 14
+      }
+    ]
+  };
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById("root"));
