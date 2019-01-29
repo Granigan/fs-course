90f396e8fbdde49a7da72f7703d6cc0b32c581c6
Tero Tapio <teromarkustapio@gmail.com>
2019-01-29 15:16:45 +0200

    2.14 - weather data

--- part_2_maat/src/component/Country.js
+++ part_2_maat/src/component/Country.js
@@ -1,18 +1,20 @@
 import React from "react";
+import Weather from "./Weather";
 
 const Country = ({ country }) => (
-      <div>
-        <h1>{country.name}</h1>
-        <div>Capital: {country.capital}</div>
-        <div>Population: {country.population}</div>
-        <h2>Languages</h2>
-        <ul>
-          {country.languages.map(language => (
-            <li key={language.iso639_1}>{language.name}</li>
-          ))}
-        </ul>
-        <img src={country.flag} alt="" width="200" />
-      </div>
-)
+  <div>
+    <h1>{country.name}</h1>
+    <div>Capital: {country.capital}</div>
+    <div>Population: {country.population}</div>
+    <h2>Languages</h2>
+    <ul>
+      {country.languages.map(language => (
+        <li key={language.iso639_1}>{language.name}</li>
+      ))}
+    </ul>
+    <img src={country.flag} alt="" width="200" />
+    <Weather city={country.capital} />
+  </div>
+);
 
 export default Country;
---
+++ part_2_maat/src/component/Weather.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+const Weather = ({ city }) => {
+  const APIKEY = process.env.REACT_APP_APIXU;
+  const [weather, setNewWeather] = useState(
+    {
+      current: {
+        temp_c: "n/a",
+        wind_kph: "n/a",
+        wind_dir: "n/a",
+        condition: {
+          icon: "#"
+        }
+      }
+    }
+  );
+
+  useEffect(() => {
+    axios
+      .get(`https://api.apixu.com/v1/current.json?key=${APIKEY}&q=${city}`)
+      .then(response => {
+        setNewWeather(response.data);
+      });
+  }, []);
+
+  console.log(weather.current);
+  console.log(weather);
+
+  return (
+    <div>
+      <h3>Weather in the capital {city}</h3>
+      <p>
+        <strong>Temperature:</strong> {weather.current.temp_c} Celsius
+      </p>
+      <img src={weather.current.condition.icon} alt="" width="100px" />
+      <p>
+        <strong>Wind:</strong> {weather.current.wind_kph} kph to {weather.current.wind_dir}
+      </p>
+    </div>
+  );
+};
+
+export default Weather;
