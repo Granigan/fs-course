20aaa92e8d37c433169a74c4f819acacec8304db
Tero Tapio <teromarkustapio@gmail.com>
2019-02-06 11:31:40 +0200

    baseurl to local

--- part_2_puhelinluettelo/src/services/persons.js
+++ part_2_puhelinluettelo/src/services/persons.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-// const baseUrl = "http://localhost:3001/api/persons";
+const baseUrl = "http://localhost:3001/api/persons";
 // const baseUrl = "https://immense-shore-56680.herokuapp.com/api/persons";
-const baseUrl = "/api/persons";
+// const baseUrl = "/api/persons";
 
 const getAll = () => {
   const request = axios.get(baseUrl);
