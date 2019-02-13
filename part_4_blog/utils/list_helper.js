e8566c46db06daf5697187d1c5deedbe1df4ca21
Tero Tapio <teromarkustapio@gmail.com>
2019-02-13 19:43:14 +0200

    4.4&4.5 - total likes&most likes

--- part_4_blog/utils/list_helper.js
+++ part_4_blog/utils/list_helper.js
@@ -2,6 +2,23 @@ const dummy = blogs => {
   return 1
 }
 
+const totalLikes = blogs => {
+  return blogs.map(blog => blog.likes).reduce((total,likes) => total+likes, 0)
+}
+
+const favouriteBlog = blogs => {
+
+  if(blogs.length < 1)  return { title: 'not available' }
+
+  return blogs.sort((a, b) => {
+    if(a.likes > b.likes) return -1
+    if(b.likes > a.likes) return 1
+    return 0
+  })[0]
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes,
+  favouriteBlog
 }
