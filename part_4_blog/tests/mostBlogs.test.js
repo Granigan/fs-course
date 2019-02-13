eef71bc0b523e673795e4c67288f9872473f407e
Tero Tapio <teromarkustapio@gmail.com>
2019-02-13 22:24:26 +0200

    4.6 - mostBlogs feat lodash

---
+++ part_4_blog/tests/mostBlogs.test.js
@@ -0,0 +1,25 @@
+const listHelper = require('../utils/list_helper')
+const testData = require('./testData')
+
+describe('most productive author', () => {
+  test('when exactly one blog', () => {
+    const blogs = [{
+      _id: '5a422a851b54a676234d17f7',
+      title: 'React patterns',
+      author: 'Michael Chan',
+      url: 'https://reactpatterns.com/',
+      likes: 7,
+      __v: 0
+    }]
+    expect(listHelper.mostBlogs(blogs)).toEqual({ author: 'Michael Chan', blogs: 1 })
+  })
+
+  test('when no blogs', () => {
+    const blogs = []
+    expect(listHelper.mostBlogs(blogs)).toEqual({ author: 'not available', blogs: 0 })
+  })
+
+  test('when all blogs', () => {
+    expect(listHelper.mostBlogs(testData.allBlogs())).toEqual({ author: 'Robert C. Martin', blogs: 3 })
+  })
+})
--- part_4_blog/utils/list_helper.js
+++ part_4_blog/utils/list_helper.js
@@ -1,24 +1,41 @@
+const lodash = require('lodash')
+
 const dummy = blogs => {
   return 1
 }
 
 const totalLikes = blogs => {
-  return blogs.map(blog => blog.likes).reduce((total,likes) => total+likes, 0)
+  return blogs
+    .map(blog => blog.likes)
+    .reduce((total, likes) => total + likes, 0)
 }
 
 const favouriteBlog = blogs => {
-
-  if(blogs.length < 1)  return { title: 'not available' }
+  if (blogs.length === 0) return { title: 'not available' }
 
   return blogs.sort((a, b) => {
-    if(a.likes > b.likes) return -1
-    if(b.likes > a.likes) return 1
+    if (a.likes > b.likes) return -1
+    if (b.likes > a.likes) return 1
     return 0
   })[0]
 }
 
+const mostBlogs = blogs => {
+  if (blogs.length === 0) return { author: 'not available', blogs: 0 }
+
+  return lodash(blogs)
+    .countBy('author')
+    .map((amount, name) => ({ author: name, blogs: amount }))
+    .sort((a, b) => b.blogs - a.blogs
+    ).first()
+}
+
+const mostLikes = blogs => {}
+
 module.exports = {
   dummy,
   totalLikes,
-  favouriteBlog
+  favouriteBlog,
+  mostBlogs,
+  mostLikes
 }
