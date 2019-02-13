0243acb7b0db08686811b1ee37cf2769cf573278
Tero Tapio <teromarkustapio@gmail.com>
2019-02-13 17:16:02 +0200

    splits model and controller for blogs

--- part_4_blog/index.js
+++ part_4_blog/index.js
@@ -3,17 +3,9 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const mongoose = require('mongoose')
+const blogsRouter = require('./controllers/blogs')
 
 
 mongoose.connect(config.mongoUrl, { useNewUrlParser: true })
@@ -21,23 +13,7 @@ mongoose.connect(config.mongoUrl, { useNewUrlParser: true })
 app.use(cors())
 app.use(bodyParser.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs/', blogsRouter)
 
 app.listen(config.PORT, () => {
   console.log(`Server running on port ${config.PORT}`)
