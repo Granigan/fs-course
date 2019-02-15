f52aef1388e6f46327222ac1797099840ef6825d
Tero Tapio <teromarkustapio@gmail.com>
2019-02-15 13:03:01 +0200

    tests test_db

--- part_4_blog/tests/blog_api.test.js
+++ part_4_blog/tests/blog_api.test.js
@@ -4,10 +4,19 @@ const app = require('../app')
 
 const api = supertest(app)
 
-test.only('all 6 blogs are returned', async () => {
-  const response = await api.get('api/blogs/')
-  console.log(response.body)
+test('notes are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('all 6 blogs are returned', async () => {
+  const response = await api
+    .get('/api/blogs')
+
   expect(response.body.length).toBe(6)
+
 })
 
 afterAll(() => {
