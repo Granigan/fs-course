30e905580ced23250a980308f71239ce79ccfb4e
Tero Tapio <teromarkustapio@gmail.com>
2019-02-17 17:10:24 +0200

    4.17 - population

--- part_4_blog/controllers/users.js
+++ part_4_blog/controllers/users.js
@@ -2,6 +2,11 @@ const bcrypt = require('bcrypt')
 const usersRouter = require('express').Router()
 const User = require('../models/user')
 
+usersRouter.get('/', async (request, response) => {
+  const users = await User.find({}).populate('blogs', { title: 1, author: 1, url: 1 })
+  response.json(users.map(user => user.toJSON()))
+})
+
 usersRouter.post('/', async (request, response, next) => {
   const body = request.body
 
@@ -28,11 +33,6 @@ usersRouter.post('/', async (request, response, next) => {
       next(error)
     }
   }
-})
-
-usersRouter.get('/', async (request, response) => {
-  const users = await User.find({})
-  response.json(users.map(user => user.toJSON()))
 })
 
 module.exports = usersRouter
