5860d682a8a27019e20b1ff69b08a6a4647328b0
Tero Tapio <teromarkustapio@gmail.com>
2019-03-01 16:56:07 +0200

    init moved to App

--- part_6_anecdotes/src/index.js
+++ part_6_anecdotes/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import anecdoteService from './services/anecdotes'
 import { createStore, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import App from './App'
-import anecdoteReducer, { initializeAnecdotes } from './reducers/anecdoteReducer'
+import anecdoteReducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
 import filterReducer from './reducers/filterReducer'
 
@@ -14,10 +13,6 @@ const reducer = combineReducers({
   filter: filterReducer
 })
 const store = createStore(reducer)
-
-anecdoteService.getAll().then(anecdotes => 
-  store.dispatch(initializeAnecdotes(anecdotes))
-)
 
 const render = () => {
   ReactDOM.render(
