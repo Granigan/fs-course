fbead384f427d36a926c5407f6117c0361af5164
Tero Tapio <teromarkustapio@gmail.com>
2019-03-01 09:43:03 +0200

    6.15 - relocated filter logic

--- part_6_anecdotes/src/components/AnecdoteList.js
+++ part_6_anecdotes/src/components/AnecdoteList.js
@@ -4,19 +4,15 @@ import { voteContent } from '../reducers/anecdoteReducer'
 import { setNotification } from '../reducers/notificationReducer'
 
 const AnecdoteList = props => {
-  const anecdotes = props.anecdotes
-
-  const filter = props.filter
-
   const vote = id => {
     props.voteContent(id)
-    const notice = `You voted for '${anecdotes.find(a => a.id === id).content}'`
+    const notice = `You voted for '${props.anecdotesToShow.find(a => a.id === id).content}'`
     props.setNotification(notice, 5000)
   }
 
   return (
     <div>
-      {anecdotes.filter(a => a.content.includes(filter)).map(anecdote =>
+      {props.anecdotesToShow.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
@@ -31,12 +27,12 @@ const AnecdoteList = props => {
   )
 }
 
-const mapStateToProps = (state) => {
-  return {
-    anecdotes: state.anecdotes,
-    filter: state.filter
-  }
-}
+const anecdotesToShow = ({ anecdotes, filter }) => 
+  anecdotes.filter(a => a.content.includes(filter))
+
+const mapStateToProps = state => ({
+  anecdotesToShow: anecdotesToShow(state)
+})
 
 const mapDispatchToProps = {
   voteContent,
