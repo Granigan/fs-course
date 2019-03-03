2bd6b9e072f04a8194170e2060a2ea87f72941f0
Tero Tapio <teromarkustapio@gmail.com>
2019-03-03 07:41:25 +0200

    6.18 - init with thunk

--- part_6_anecdotes/src/App.js
+++ part_6_anecdotes/src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
-import anecdoteService from './services/anecdotes'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
@@ -9,8 +8,7 @@ import { initializeAnecdotes } from './reducers/anecdoteReducer'
 
 const App = (props) => {
   useEffect(() => {
-    anecdoteService
-      .getAll().then(anecdotes => props.initializeAnecdotes(anecdotes))
+    props.initializeAnecdotes()
   }, [])
 
   return (
